76f44634ae6aa781122f4081a1a6cad2ea4765ea
Ishtiaque Akbar <136613343+Akbari06@users.noreply.github.com>
2024-11-23 20:07:14 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // JavaScript functionality can be added here
-    console.log("Tutor website loaded.");
-});
-
-function handleFindTutor() {
-    alert("Thank you for your interest! Please fill out the form below to get started.");
-}
